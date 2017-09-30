c24c40e1c384db70a7fdff034f8473c4566b7d86
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-30 17:49:12 +0100

    first commit, shows linq and query method differences

--- WorkingWithLINQ/Program.cs
+++ WorkingWithLINQ/Program.cs
@@ -4,12 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WorkingWithLINQ
+namespace UnderstandingLINQ
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<Car> myCars = new List<Car>() {
+                new Car() { VIN="A1", Make = "BMW", Model= "550i", StickerPrice=55000, Year=2009},
+                new Car() { VIN="B2", Make="Toyota", Model="4Runner", StickerPrice=35000, Year=2010},
+                new Car() { VIN="C3", Make="BMW", Model = "745li", StickerPrice=75000, Year=2008},
+                new Car() { VIN="D4", Make="Ford", Model="Escape", StickerPrice=25000, Year=2008},
+                new Car() { VIN="E5", Make="BMW", Model="55i", StickerPrice=57000, Year=2010}
+            };
+            /*
+            //LINQ Query
+            var BMWS = from car in myCars
+                       where car.Make == "BMW"
+                       && car.Year == 2010
+                       select car;
+            
+            var orderedCars = from car in myCars
+                              orderby car.Year descending
+                              select car;
+
+            
+            //LINQ Method
+            //lamb expression, given an instance of the expression, return back to me only
+            those instances where the make is equal to BMW.
+            a mini method
+            
+            var orderedCars =
+            
+            var firstBMW = myCars.OrderByDescending(p => p.Year).First(p => p.Make == "BMW");
+            Console.WriteLine(firstBMW);
+            */
+
+            Console.WriteLine()
+
+            var BMWS = myCars.Where(p => p.Make == "BMW" && p.Year == 2010);
+
+            foreach (var car in BMWS)
+            {
+                Console.WriteLine("{0} {1}", car.Model, car.VIN);
+            }
+            */
+            Console.ReadLine();
         }
+    }
+
+    class Car
+    {
+        public string VIN { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double StickerPrice { get; set; }
     }
 }
